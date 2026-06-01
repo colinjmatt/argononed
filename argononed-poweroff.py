8253f7ee1730718ca601cfc64f63e1ef7fc727fb
Colin Matthews <colinjmatt@users.noreply.github.com>
2026-06-02 00:22:20 +0100

    Update argononed-poweroff.py

--- argononed-poweroff.py
+++ argononed-poweroff.py
@@ -1,19 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import smbus
-import RPi.GPIO as GPIO
 
-rev = GPIO.RPI_REVISION
-if rev == 2 or rev == 3:
+# The Raspberry Pi 4 uses I2C bus 1
+try:
     bus = smbus.SMBus(1)
-else:
-    bus = smbus.SMBus(0)
+except Exception as e:
+    print(f"Error initializing SMBus 1: {e}")
+    try:
+        bus = smbus.SMBus(0)
+    except:
+        bus = None
 
-if len(sys.argv) > 1:
-    bus.write_byte_data(0x1a,0,0)
-    if sys.argv[1] == "poweroff" or sys.argv[1] == "halt":
+if bus is not None and len(sys.argv) > 1:
+    # First, tell the Argon MCU to stop the fan (send 0 to address 0x1a)
+    try:
+        bus.write_byte_data(0x1a, 0, 0)
+    except IOError:
+        pass
+        
+    # If systemd is halting or powering off the OS, tell the MCU to physically cut power
+    if sys.argv[1] in ["poweroff", "halt"]:
         try:
-            bus.write_byte_data(0x1a,0,0xFF)
-        except:
-            rev = 0
+            # 0xFF signals the Argon MCU to sever the power connection
+            bus.write_byte_data(0x1a, 0, 0xFF)
+        except IOError:
+            pass
